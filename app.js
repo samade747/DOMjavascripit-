df30d33bc650ed6f4441d3717e24b7f6a29a8c6a
samade747 <samad.e747@gmail.com>
2023-10-14 02:16:12 +0500

    xxdzx

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const color = ['red', 'blue', "green", 'yellow', 'pink', 'violet'];
+// const color = ['red', 'blue', "green", 'yellow', 'pink', 'violet'];
 
-var colorName = document.querySelector('span');
+// var colorName = document.querySelector('span');
 
 
-function colorHandler(){
-    const randomGeneratedNUmber = Math.floor(Math.random() * 6);
-    console.log(randomGeneratedNUmber);
-    colorName.textContent = color[randomGeneratedNUmber];
-    document.body.style.background = color[randomGeneratedNUmber];    
+// function colorHandler(){
+//     const randomGeneratedNUmber = Math.floor(Math.random() * 6);
+//     console.log(randomGeneratedNUmber);
+//     colorName.textContent = color[randomGeneratedNUmber];
+//     document.body.style.background = color[randomGeneratedNUmber];    
 
+// }
+
+const colors = ['red', 'blue', 'green', 'yellow', 'pink', 'violet']
+
+var colorName = document.querySelector('span')
+
+
+function colorHandler() {
+    const randomGeneratedNumber = Math.floor(Math.random() * 6)
+    console.log("chal gaya")
+    console.log(randomGeneratedNumber)
+    colorName.textContent = colors[randomGeneratedNumber]
+    document.body.style.backgroundColor = colors[randomGeneratedNumber]
 }
